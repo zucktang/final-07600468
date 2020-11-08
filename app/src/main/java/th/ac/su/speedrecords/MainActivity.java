8a484d855b336fec0ca59387bf2d18bcb14acd95
Nontaphat Noijai <zucktang@gmail.com>
2020-11-08 12:08:57 +0700

    --

--- app/src/main/java/th/ac/su/speedrecords/MainActivity.java
+++ app/src/main/java/th/ac/su/speedrecords/MainActivity.java
@@ -1,14 +1,29 @@
 package th.ac.su.speedrecords;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button add_record = (Button)findViewById(R.id.add_record);
+        add_record.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent x = new Intent(MainActivity.this , MainActivity2.class);
+                        startActivity(x);
+
+            }
+        });
     }
 }
--- app/src/main/java/th/ac/su/speedrecords/MainActivity2.java
+++ app/src/main/java/th/ac/su/speedrecords/MainActivity2.java
@@ -1,14 +1,53 @@
 package th.ac.su.speedrecords;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity2 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Button save = findViewById(R.id.save) ;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText distance = findViewById(R.id.distance);
+                EditText time = findViewById(R.id.time);
+
+                String Distance = distance.getText().toString();
+                String Time = time.getText().toString();
+                if (Distance.isEmpty() || Time.isEmpty()) {
+                    Toast t = Toast.makeText(MainActivity2.this, R.string.Distanceandtimearerequired, Toast.LENGTH_LONG);
+                    t.show();
+                } else {
+                    Double X = Double.parseDouble(Distance);
+                    Double Y = Double.parseDouble(Time);
+                    if (Y == 0) {
+                        Toast t = Toast.makeText(MainActivity2.this, R.string.Timemustbegreaterthanzero, Toast.LENGTH_LONG);
+                        t.show();
+                    } else {
+                        Double ANS = (X / 1000) / (Y / 3600);
+
+                        if (ANS > 80) {
+                            AlertDialog.Builder dialog = new AlertDialog.Builder(MainActivity2.this);
+                            dialog.setTitle("SPEED CALCULATOR");
+                            dialog.setMessage(R.string.Speedisoverlimit);
+                            dialog.setPositiveButton("OK", null);
+                            dialog.show();
+                        }
+                    }
+                }
+            }
+        });
+
+        }
     }
-}
